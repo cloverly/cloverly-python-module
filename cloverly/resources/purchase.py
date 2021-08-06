e2239d53482df3ad9845b37e3eb6c26b6e4d0a0e
zainlks <lakhani.zain786@gmail.com>
2021-08-06 01:07:17 -0400

    Resources: Create Purchase Resource Object

---
+++ cloverly/resources/purchase.py
@@ -0,0 +1,9 @@
+from cloverly.base import CloverlyResource
+
+
+class Purchase(CloverlyResource):
+    resource_url = 'purchases'
+
+    @classmethod
+    def Estimate(cls, **kwargs) -> CloverlyResource:
+        return cls.extend_endpoint("", **kwargs)
