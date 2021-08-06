3f6a12879acbf55ad831191c40abfe3172033976
zainlks <lakhani.zain786@gmail.com>
2021-08-06 01:06:40 -0400

    Resources: Create base resource object

---
+++ cloverly/base.py
@@ -0,0 +1,80 @@
+import requests
+
+
+class CloverlyResource:
+    base_url = 'https://api.cloverly.com'
+    resource_url = ''
+    _version = ''
+    _headers = {}
+
+    def __init__(self, **kwargs: iter):
+        for attr in kwargs:
+            self.__setattr__(attr, kwargs[attr])
+
+    @classmethod
+    def request(cls, url: str, method: str, data: dict) -> requests.request:
+        r = requests.request(method, url, json=data, headers=cls._headers)
+        return r
+
+    @classmethod
+    def list(cls, **kwargs):
+        r = cls.request(f"{cls.base_url}/{cls._version}/{cls.resource_url}", 'GET', kwargs)
+        results = r.json()
+        if type(results) == list:
+            object_list = []
+            for result in results:
+                new = cls(**result)
+                object_list.append(new)
+            return object_list
+
+    @classmethod
+    def activate_session(cls, api_key, version):
+        cls._version = version
+        cls._headers["Authorization"] = f"Bearer {api_key}"
+        cls._headers["Content-type"] = "application/json"
+
+    @classmethod
+    def extend_endpoint(cls, endpoint: str, **kwargs):
+        e = cls(**kwargs)
+        e.save(endpoint)
+        return e
+
+    @classmethod
+    def Fixed(cls, **kwargs):
+        return cls.extend_endpoint("/currency", **kwargs)
+
+    @classmethod
+    def Carbon(cls, **kwargs):
+        return cls.extend_endpoint("/carbon", **kwargs)
+
+    @classmethod
+    def Shipping(cls, **kwargs):
+        return cls.extend_endpoint("/shipping", **kwargs)
+
+    @classmethod
+    def Ground(cls, **kwargs):
+        return cls.extend_endpoint("/vehicle", **kwargs)
+
+    @classmethod
+    def Flight(cls, **kwargs):
+        return cls.extend_endpoint("/flights", **kwargs)
+
+    @classmethod
+    def Electricity(cls, **kwargs):
+        return cls.extend_endpoint("/electricity", **kwargs)
+
+    def delete(self):
+        url = f"{self.base_url}/{self._version}/{self.resource_url}/{self.__getattribute__('slug')}"
+        self.request(url, "DELETE", {})
+
+    def save(self, slug: str = None):
+        url = f"{self.base_url}/{self._version}/{self.resource_url}"
+
+        if slug is not None:
+            url += slug
+
+        r = self.request(url, "POST", self.__dict__)
+        result = r.json()
+
+        for attr in result:
+            self.__setattr__(attr, result[attr])
